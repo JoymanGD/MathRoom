02014a526db565b788c728603e78a2b57d9b605f
JoymanGD <joyman.gamedev@gmail.com>
2021-06-17 19:07:29 +1000

    ballistic updated

--- Scenes/Ballistics.cs
+++ Scenes/Ballistics.cs
@@ -12,15 +12,25 @@ namespace MathRoom.Scenes
     public class Ballistic : IScene
     {
         //constant
+        private const float ParabolaHeight = 200;
         private const float FloorHeight = 50;
+        private const float Gravity = 9.8f;
         private readonly Size2 CannonSize = new Size2(20, 40);
+        private const float StartSpeed = 12f;
 
         //scalable
-        private float CannonX = 40;
+        private Vector2 CannonPosition = new Vector2(40, 0);
+        private Vector2 BallPosition = new Vector2(40, 0);
         private float CannonMovingSpeed = 2;
+        private bool Thrown = false;
 
         //calculatable
-        private Vector2 FloorStart, FloorEnd, Cannon;
+        private Vector2 FloorStart, FloorEnd;
+        private Vector2 MousePosition;
+        private Vector2 Peek, Direction;
+        private float Time;
+        private float MiddlePoint;
+        private float Max;
 
         public override void Initialize()
         {
@@ -39,7 +49,7 @@ namespace MathRoom.Scenes
         }
 
         private void SetCanon(){
-            Cannon = new Vector2(CannonX, FloorStart.Y - CannonSize.Height);
+            CannonPosition = new Vector2(CannonPosition.X, FloorStart.Y - CannonSize.Height);
         }
 
         public override void Update(GameTime _gameTime)
@@ -48,17 +58,41 @@ namespace MathRoom.Scenes
             var mouseState = MouseExtended.GetState();
             
             if(keyState.IsKeyDown(Keys.D)){
-                CannonX += CannonMovingSpeed;
+                CannonPosition.X += CannonMovingSpeed;
             }
             if(keyState.IsKeyDown(Keys.A)){
-                CannonX -= CannonMovingSpeed;
+                CannonPosition.X -= CannonMovingSpeed;
             }
 
             if(mouseState.DeltaScrollWheelValue != 0){
                 CannonMovingSpeed = Math.Clamp(CannonMovingSpeed - mouseState.DeltaScrollWheelValue * .001f, 0, 10);
             }
 
-            Cannon.X = CannonX;
+            if(mouseState.WasButtonJustDown(MouseButton.Left)){
+                BallPosition = CannonPosition;
+                MousePosition = mouseState.Position.ToVector2();
+                MiddlePoint = (MousePosition.X + BallPosition.X) / 2;
+                Peek = new Vector2(MiddlePoint, ParabolaHeight);
+                Direction = (Peek - CannonPosition).NormalizedCopy();
+                Time = MathF.Abs(MousePosition.X - BallPosition.X) / StartSpeed;
+                Max = MiddlePoint - BallPosition.X;
+                
+                Thrown = true;
+            }
+
+            if(Thrown){
+                var deltaTime = (float)_gameTime.ElapsedGameTime.TotalSeconds;
+                var finish = new Vector2(MousePosition.X, Graphics.PreferredBackBufferHeight - FloorHeight);
+
+                BallPosition = Parabola(BallPosition, finish, ParabolaHeight, deltaTime);
+
+                if(BallPosition == finish){
+                    Thrown = false;
+                }
+            }
+            else{
+                BallPosition = CannonPosition;
+            }
 
             AdditionalInfo = "Move cannon: A-D\nChange cannon moving speed: MouseWheel\nShoot: LMB\nCannon moving speed: " + MathF.Round(CannonMovingSpeed, 3);
         }
@@ -68,8 +102,22 @@ namespace MathRoom.Scenes
             GraphicsDevice.Clear(Color.Black);
             _spriteBatch.Begin();
             _spriteBatch.DrawLine(FloorStart, FloorEnd, Color.White, 2); //floor
-            _spriteBatch.DrawRectangle(Cannon, CannonSize, Color.SeaGreen, CannonSize.Width); //cannon
+            _spriteBatch.DrawCircle(BallPosition + new Vector2(CannonSize.Width/2, 0), CannonSize.Width/2, 10, Color.Blue, CannonSize.Width/2); //ball
+            _spriteBatch.DrawRectangle(CannonPosition, CannonSize, Color.SeaGreen, CannonSize.Width); //cannon
+            _spriteBatch.DrawLine(CannonPosition, CannonPosition+Direction * 40, Color.Red, 2); //floor
             _spriteBatch.End();
+        }
+
+        public Vector2 Parabola(Vector2 start, Vector2 end, float height, float deltaTime)
+        {
+            var deltaX = Normalize(MiddlePoint - start.X, -Max, Max);
+            var deltaY = Vector2.UnitY * deltaX;
+
+            return Vector2.Lerp(start, end, deltaTime) - deltaY * height;
+        }
+
+        private float Normalize(float value, float min, float max){
+            return (value - min) / (max - min);
         }
 
         public override void Reset()
